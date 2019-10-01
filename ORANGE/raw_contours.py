405590f28f0206e0b413aa41908c2d29ca6c3222
MinhDua <MinhDua@gmail.com>
2019-10-01 20:09:52 +0700

    filter orange

--- ORANGE/raw_contours.py
+++ ORANGE/raw_contours.py
@@ -30,16 +30,53 @@ def changeBilateral():
 	while True:
 		blur = cv2.bilateralFilter(img,x,y,z)
 		cv2.imshow('Bilateral',blur)
-		k = cv2.waitKey(0)
+		k = cv2.waitKey(1)
 		if k == 27:
 			break
-
+def changeMask():
+	x0,x1,y0,y1,z0,z1= 6,31,171,255,171,255
+	cv2.namedWindow("Mask")
+	hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+	cv2.createTrackbar("x0","Mask",x0,255,nothing)
+	cv2.createTrackbar("y0","Mask",y0,255,nothing)
+	cv2.createTrackbar("z0","Mask",z0,255,nothing)
+	cv2.createTrackbar("x1","Mask",x1,255,nothing)
+	cv2.createTrackbar("y1","Mask",y1,255,nothing)
+	cv2.createTrackbar("z1","Mask",z1,255,nothing)
+	while True:
+		x0 = cv2.getTrackbarPos('x0',"Mask")
+		y0 = cv2.getTrackbarPos('y0',"Mask")
+		z0 = cv2.getTrackbarPos('z0',"Mask")
+		x1 = cv2.getTrackbarPos('x1',"Mask")
+		y1 = cv2.getTrackbarPos('y1',"Mask")
+		z1 = cv2.getTrackbarPos('z1',"Mask")
+		low = np.array([x0,y0,z0])
+		upper = np.array([x1,y1,z1])
+		mask = cv2.inRange(hsv,low,upper)
+		result = cv2.bitwise_and(img,img,mask=mask)
+		cv2.imshow('Result',result)
+		cv2.imshow('Mask',mask)
+		
+		k = cv2.waitKey(1)
+		if k == 27:
+			break
+def testMask():
+	cv2.namedWindow("Mask")
+	hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+	low = np.array([80,0,0])
+	upper = np.array([112,255,255])
+	mask = cv2.inRange(hsv,low,upper)
+	#mask = cv2.bitwise_and(img,img,mask=mask)
+	cv2.imshow('Mask',mask)
+	k = cv2.waitKey(0)
 img = cv2.imread('orange.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 def Main():
-	x =1
-	cv2.namedWindow('Thresh')
-	changeBilateral()
+	#x =1
+	#testMask()
+	changeMask()
+	#cv2.namedWindow('Thresh')
+	#changeBilateral()
 	#cv2.createTrackbar("Threshold","Thresh",x,255,changeBilateral)
 	# read a image from computer
 	# blur image
@@ -47,7 +84,7 @@ def Main():
 	# convert to grayscale
 	# covert to binary image
 	# b
-	changeBlur()
+	#changeBlur()
 	cv2.imshow('gray',gray)
 	cv2.imshow('Contours Orange',img)
 	k = cv2.waitKey(0)
